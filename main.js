a447230d1513a0c3213c1cc6bbd8e341937984e2
Ashley Claiborne <languageisaliquid@gmail.com>
2019-09-12 22:24:13 -0500

    created print to dom func

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-console.log('it works!');
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
 
 const toCelsius =  () => {
 
